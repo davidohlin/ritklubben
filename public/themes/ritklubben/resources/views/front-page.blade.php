f5f9ef39f90f7b71d7bed09039460e9751025e01
davidohlin <hello@davidohl.in>
2018-03-28 22:35:08 +0200

    add grid to front-page

--- public/themes/ritklubben/resources/views/front-page.blade.php
+++ public/themes/ritklubben/resources/views/front-page.blade.php
@@ -5,9 +5,22 @@
 @extends('layouts.app')
 
 @section('content')
-
-  @while (have_posts()) @php(the_post())
-    @include('partials.content-'.get_post_type())
-  @endwhile
-
+<div class="container mx-auto">
+	<div class="clearfix">
+		<div class="col col-12">
+			@if (!have_posts())
+			<div class="alert alert-warning">
+				{{ __('Sorry, no results were found.', 'sage') }}
+			</div>
+				{!! get_search_form(false) !!}
+			@endif
+	
+			@foreach ($posts as $post)
+				<div class="col col-12 md-col-6 px2">
+					@include('partials.excerpt')
+				</div>
+			@endforeach
+		</div>
+	</div>
+</div>
 @endsection
