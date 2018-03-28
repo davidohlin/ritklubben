28e1e27334c136016599f42a0d47c4017d51242f
davidohlin <hello@davidohl.in>
2018-03-28 22:34:43 +0200

    add contact template

---
+++ public/themes/ritklubben/resources/views/contact.blade.php
@@ -0,0 +1,21 @@
+{{--
+  Template Name: Contact
+--}}
+
+@extends('layouts.app')
+
+@section('content')
+<div class="container mx-auto">
+	<div class="clearfix">
+		<div class="col col-12 md-col-6 px2">
+			<img src="{{ $featured_image['url'] }}" alt="{{ $featured_image['alt'] }}">
+		</div>
+		<div class="col-right col-12 md-col-6 px2" style="font-size:%;">
+		@while(have_posts()) @php(the_post())
+			@include('partials.content-page')
+		@endwhile
+		</div>
+	</div>
+</div>
+
+@endsection
