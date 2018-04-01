0f4ef2d6793ec17e5b3fea8414153b6755059428
davidohlin <hello@davidohl.in>
2018-04-01 20:37:35 +0200

    add default link color 🔵🔵🔵

--- public/themes/ritklubben/resources/views/partials/excerpt.blade.php
+++ public/themes/ritklubben/resources/views/partials/excerpt.blade.php
@@ -2,7 +2,7 @@
 
 <article class="excerpt">
 	@if (has_post_thumbnail($post->ID))
-	<a href="{{ get_permalink($post->ID) }}">
+	<a class="img-link" href="{{ get_permalink($post->ID) }}">
 		<div class="img-container">
 			<img alt="{{ $post->post_title }}" class="lozad" data-src="{!! get_the_post_thumbnail_url($post->ID) !!}">
 		</div>
