9e075f297d217bb226fd9cba2852f8f58bd95cf5
davidohlin <hello@davidohl.in>
2018-04-02 01:02:08 +0200

    add footer

--- public/themes/ritklubben/resources/views/partials/footer.blade.php
+++ public/themes/ritklubben/resources/views/partials/footer.blade.php
@@ -1,4 +1,13 @@
-<footer class="content-info">
-  <div class="container">
-  </div>
+<footer class="py4">
+	<div class="container mx-auto">
+		<div class="clearfix">
+			<div class="col col-12 px3 center">
+				<div class="footer__heart">
+					@include('partials.heart')
+				</div>
+				<span class="strong">Ritklubben {{ date("Y") }}</span><br>
+				<a class="strong" href="mailto:ritklubben@gmail.com">ritklubben@gmail.com</a>
+			</div>
+		</div>
+	</div>
 </footer>
