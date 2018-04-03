ed55d9347d36d0ba34165a97753bc31702c69d0c
davidohlin <hello@davidohl.in>
2018-04-03 22:53:12 +0200

    clean up index

--- public/themes/ritklubben/resources/views/index.blade.php
+++ public/themes/ritklubben/resources/views/index.blade.php
@@ -1,18 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
-
-  @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, no results were found.', 'sage') }}
+<div class="container mx-auto">
+  <div class="clearfix">
+    <div class="col col-12 md-col-6 px3 mb3">
+      <h1 class="mb2">{{ $post->post_title }}</h1>
+      @include('partials.author')
+      @while(have_posts()) @php(the_post())
+        @include('partials.content-page')
+      @endwhile
     </div>
-    {!! get_search_form(false) !!}
-  @endif
-
-  @while (have_posts()) @php(the_post())
-    @include('partials.content-'.get_post_type())
-  @endwhile
-
-  {!! get_the_posts_navigation() !!}
+  </div>
+</div>
 @endsection
