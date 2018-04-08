6b184fe2c41dc443cd8a7840d24b89162f1e4516
davidohlin <hello@davidohl.in>
2018-04-08 12:41:20 +0200

    working dates

--- public/themes/ritklubben/app/controllers/dates.php
+++ public/themes/ritklubben/app/controllers/dates.php
@@ -9,36 +9,83 @@ use DateTimeZone;
 class Dates extends Controller
 {
 
-    /*
-     * Returns array of dates
-     * @return array
-     */
+	/*
+	 * Returns array of dates
+	 * @return array
+	 */
 	public function dates()
 	{	
 		$dates = get_field('dates');
-		
 		$arr = [];
 
 		foreach ($dates as $key => $value) {
-			$arr[$key]['date'] = $value['date'];
+			$arr[$key]['weekday'] = self::getWeekday(date('l', strtotime($value['date'])));
+			$arr[$key]['date'] = date('j', strtotime($value['date']));
+			$arr[$key]['month'] = self::getMonth(date('F', strtotime($value['date'])));
+			$arr[$key]['time_starts'] = $value['time_starts'];
+			$arr[$key]['time_ends'] = $value['time_ends'];
+			$arr[$key]['location'] = $value['location'];
 			$arr[$key]['has_passed'] = self::dateHasPassed($value['date']);
 		}
 
 		return $arr;
 	}
 
-    /*
+	/*
      * Returns true if the date has passed
      * @return boolean
      */
 	private function dateHasPassed($date)
 	{
-		
 		$timezone = new DateTimeZone('Europe/Stockholm');
 		$now = new DateTime('now', $timezone);
-		$now = $now->format('Y-m-d H:i:s');
-
+		$now = $now->format('m/d/Y');
+		
 		return $now > $date;
+	}
+
+	/*
+	 * Get the weekday from a date string
+	 * @return string
+	 */
+	private function getWeekday($weekday)
+	{
+		$weekdays = array(
+			'Monday' => 'Måndag', 
+			'Tuesday' => 'Tisdag', 
+			'Wednesday' => 'Onsdag',
+			'Thursday' => 'Torsdag',
+			'Friday' => 'Fredag', 
+			'Saturday' => 'Lördag',
+			'Sunday' => 'Söndag'
+		);
+
+		return $weekdays[$weekday];
+	}
+
+	/*
+	 * Get the month from a date string
+	 * @return string
+	 */
+	private function getMonth($month)
+	{
+		$months = array(
+			'January' => 'Januari', 
+			'February' => 'Februari', 
+			'Mars' => 'Mars', 
+			'April' => 'April',
+			'May' => 'Maj',
+			'June' => 'Juni', 
+			'July' => 'Juli',
+			'August' => 'Augusti',
+			'September' => 'September',
+			'October' => 'Oktober',
+			'November' => 'November',
+			'December' => 'December',
+		);
+
+		return $months[$month];
+
 	}
 
 }
--- public/themes/ritklubben/resources/views/dates.blade.php
+++ public/themes/ritklubben/resources/views/dates.blade.php
@@ -10,8 +10,9 @@
 	<div class="clearfix">
  		@foreach($dates as $date)
 		<div class="col col-12 px3 mb3 center">
-        	<h2 class="date @if($date['has_passed']) has-passed @endif">{{ $date['date'] }}</h2>
-        	<p>Beckmans Designhögskola<br>Brahegatan 10,Stockholm</p>
+        	<h2 class="date @if($date['has_passed']) has-passed @endif">{{ $date['weekday'] }} {{ $date['date'] }} {{ $date['month'] }}</h2>
+        	<p>{{ $date['time_starts'] }} @if($date['time_ends']) – {{ $date['time_ends'] }} @endif</p>
+        	<p>Beckmans Designhögskola</p>
     	</div>
  		@endforeach 
 	</div>
