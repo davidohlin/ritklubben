3f6b539805b3f9d572c30ea3e88cba332c5d1576
davidohlin <hello@davidohl.in>
2018-05-16 19:58:28 +0200

    update info

--- public/themes/ritklubben/resources/views/contact.blade.php
+++ public/themes/ritklubben/resources/views/contact.blade.php
@@ -10,9 +10,9 @@
 		<div class="col col-12 md-col-4 px3">
 			<img class="lozad mb3" data-src="{{ $featured_image['url'] }}" alt="{{ $featured_image['alt'] }}">
 			<ul>
-				<li><a class="strong" href="mailto:ritklubben@gmail.com">ritklubben@gmail.com</a></li>
-				<li><a class="strong" href="#">Instagram</a></li>
-				<li><a class="strong" href="#">Facebook</a></li>
+				<li><a class="strong" href="mailto:ritklubb@gmail.com">ritklubb@gmail.com</a></li>
+				<li><a class="strong" href="https://www.instagram.com/ritklubb/">Instagram</a></li>
+				<li><a class="strong" href="https://www.facebook.com/ritklubben">Facebook</a></li>
 			</ul>
 		</div>
 		<div class="col-right col-12 md-col-8 px3">
